743affe56b09b2c6e32516bbb17c75837fdb642e
saisampathkumar <42651479+saisampathkumar@users.noreply.github.com>
2018-09-16 10:54:04 -0500

    Lab Assignment

--- LabAssignment_1/home/home.controller.js
+++ LabAssignment_1/home/home.controller.js
@@ -6,7 +6,8 @@
         .controller('HomeController', HomeController);
 
     HomeController.$inject = ['UserService', '$rootScope'];
-    function HomeController(UserService, $rootScope) {
+    function HomeController(UserService, $rootScope)
+    {
         var vm = this;
 
         vm.user = null;
@@ -15,28 +16,35 @@
 
         initController();
 
-        function initController() {
+        function initController()
+        {
             loadCurrentUser();
             loadAllUsers();
         }
 
-        function loadCurrentUser() {
+        function loadCurrentUser()
+        {
             UserService.GetByUsername($rootScope.globals.currentUser.username)
-                .then(function (user) {
+                .then(function (user)
+                {
                     vm.user = user;
                 });
         }
 
-        function loadAllUsers() {
+        function loadAllUsers()
+        {
             UserService.GetAll()
-                .then(function (users) {
+                .then(function (users)
+                {
                     vm.allUsers = users;
                 });
         }
 
-        function deleteUser(id) {
+        function deleteUser(id)
+        {
             UserService.Delete(id)
-            .then(function () {
+            .then(function ()
+            {
                 loadAllUsers();
             });
         }
