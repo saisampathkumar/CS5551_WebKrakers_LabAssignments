146889d8045249042e1d6a6f4cbb80a1a8b7e24f
veereshthotigar <veeresh.thotygar@gmail.com>
2018-10-10 20:21:00 -0500

    tips api

--- LabAssignment_2/hybridApp/src/pages/home/home.ts
+++ LabAssignment_2/hybridApp/src/pages/home/home.ts
@@ -11,24 +11,21 @@ export class HomePage {
   public searchKeyword : String;
   public tips: Observable<any>;
   public url : any;
+  client_id = 'NTAUYPH1YIVXKZO4IHAFMBEQKJGDZ5URV2NRJJXYK2ZTLZZE';
+  client_secret = 'FZNYKYV4JDBSZMKSPQNHFDZWMFRR114WALWHRPGGV3YOTBXR';
   constructor(public navCtrl: NavController,private http: HttpClient) {
 
   }
   getdetails(){
-
-    this.city = 'Kansas city';
-    this.searchKeyword = 'Airport';
-    this.url = 'https://api.foursquare.com/v2/tips/search?client_id=NTAUYPH1YIVXKZO4IHAFMBEQKJGDZ5URV2NRJJXYK2ZTLZZE&client_secret=FZNYKYV4JDBSZMKSPQNHFDZWMFRR114WALWHRPGGV3YOTBXR&v=20160215&limit=5&near='+this.city+'&query='+this.searchKeyword;
-    console.log(this.url);
+    this.url = 'https://api.foursquare.com/v2/tips/search?client_id='+this.client_id+'&client_secret='+this.client_secret+'&v=20160215&limit=5&near='+this.city+'&query='+this.searchKeyword;
     this.http.get(this.url)
       .subscribe(
-        (res:Observable<any>)=>{
-          this.tips = res.response.tips;
-          console.log(this.tips);
+        (res)=>{
+          this.tips = res.response.tips;console.log(this.tips);
         }
       )
     }
-  ngOnInit() {
-      this.getdetails();
-    }
+  // ngOnInit() {
+  //     this.getdetails();
+  //   }
 }
